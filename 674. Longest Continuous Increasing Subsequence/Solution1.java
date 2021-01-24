c48faf0343a0b68c86abf7b308ae2c6fd51b60cb
Cecilia <sunrui7337@gmail.com>
2021-01-23 22:31:08 -0500

    Comment on solution1

--- 674. Longest Continuous Increasing Subsequence/Solution1.java
+++ 674. Longest Continuous Increasing Subsequence/Solution1.java
@@ -1,4 +1,8 @@
 public class Solution1 {
+    //Runtime: 1 ms, faster than 98.11%
+    //Memory Usage: 39.3 MB, less than 98.42%
+    //Complexity: O(n) 
+
     public int findLengthOfLCIS(int[] nums) {
         if (nums.length == 1){
             return 1;
@@ -7,7 +11,9 @@ public class Solution1 {
         int max = 0;
         
         for(int i = 0;i < nums.length - 1;){
-            int count = 1;
+            //loop through all integers
+            
+            int count = 1;      //to store each stage of the number of consecutive integers
             while(nums[i + 1] > nums[i]){
                 count ++;
                 if(i < nums.length - 2){
@@ -17,6 +23,7 @@ public class Solution1 {
                 }
             }
             
+            //largest amount of consecutive integers
             if(count > max){
                 max = count;
             }
