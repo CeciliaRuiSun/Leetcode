79d223dd2e686f045e1942f57602d30cdb008dce
Cecilia <sunrui7337@gmail.com>
2021-03-20 15:43:45 -0400

    solution

--- Merge Intervals/57InsertInterval.java
+++ Merge Intervals/57InsertInterval.java
@@ -1,44 +1,53 @@
 class Solution {
+    // Runtime: 1 ms, faster than 98.83%
+    // Memory Usage: 41.5 MB, less than 43.47%
+    
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> ret = new ArrayList();
-        int size = intervals.length;
+        int len = intervals.length;
         
-        if(size == 0){
-            ret.add(newInterval);
-            return ret.toArray(new int[ret.size()][]);
-        }
+        int c = newInterval[0];
+        int d = newInterval[1];
+        boolean find = false;
         
         int i = 0;
-        int a = newInterval[0];
-        int b = newInterval[1];
-        while(i < size){
-            if(intervals[i][1] < a){
-                ret.add(intervals[i]);
-                i++;
-                continue;
-            } else{
-                ret.add(new int[]{0,0});
-                //System.out.println(Arrays.toString(ret.get(ret.size()-1)));
-                ret.get(ret.size()-1)[0] = intervals[i][0];
-                i++;
-                while(i < size && intervals[i][0] < b){
-                    i++;
-                }
-                
-                if(i >= size){
-                    ret.get(ret.size()-1)[1] = Math.max(b, intervals[i-1][1]);
-                } else if(intervals[i][0] == b){
-                    ret.get(ret.size()-1)[1] = intervals[i][1];
-                    i++;
-                } else if(intervals[i][0] > b){
-                    ret.get(ret.size()-1)[1] = b;
-                }
+        while(i < len){
+            int a = intervals[i][0];
+            int b = intervals[i][1];
+            if(d < a){
+                ret.add(newInterval);
+                find = true;
+                i --;
                 break;
-            }            
+            }
+            
+            if(c <= a && a <= d || a <= c && c <= b){
+                ret.add(new int[]{Math.min(a,c), Math.max(b,d)});
+                find = true;
+                break;
+            } 
+            else{
+                ret.add(intervals[i]);
+            }
+            i ++;
         }
         
-        for(int j = i; j < size; j++){
-         ret.add(intervals[j]);
+        if(!find){
+            ret.add(newInterval);
+        } else if(i < len - 1){
+            while(i < len - 1){
+                i ++;
+                //System.out.println("ret : " + ret.get(ret.size() - 1)[1]);
+                //System.out.println(intervals[i][0]);
+                //System.out.println(intervals[i][1]);
+                if(ret.get(ret.size() - 1)[1] >= intervals[i][1]){
+
+                } else if(ret.get(ret.size() - 1)[1] < intervals[i][0]){
+                    ret.add(intervals[i]);
+                } else{
+                    ret.get(ret.size() - 1)[1] = intervals[i][1];
+                }
+            }
         }
         
         return ret.toArray(new int[ret.size()][]);
