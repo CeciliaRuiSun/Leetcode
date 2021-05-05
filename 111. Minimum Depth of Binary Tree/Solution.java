0e4db0ad3a43a50a3c6a42705296bdc86ff318b8
Cecilia <sunrui7337@gmail.com>
2021-05-05 14:27:04 -0400

    solution

---
+++ 111. Minimum Depth of Binary Tree/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        
+        if(root.left == null || root.right == null){
+            return Math.max(minDepth(root.left),minDepth(root.right)) + 1;
+        }
+        
+        return Math.min(minDepth(root.left),minDepth(root.right)) + 1;
+    }
+}
