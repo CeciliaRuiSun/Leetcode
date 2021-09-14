462ec59c852b3e0dad16cbbc70bea54cf4959e62
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2021-09-13 20:03:07 -0700

    Create solution2.java
    use hashmap

---
+++ 13. Roman to Integer/solution2.java
@@ -0,0 +1,47 @@
+// Runtime: 8 ms, faster than 27.56%
+// Memory Usage: 38.9 MB, less than 90.09%
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<String,Integer> map = new HashMap<>();
+        map.put("I",1);
+        map.put("V",5);
+        map.put("X",10);
+        map.put("L",50);
+        map.put("C",100);
+        map.put("D",500);
+        map.put("M",1000);
+        map.put("IV",4);
+        map.put("IX",9);
+        map.put("XL",40);
+        map.put("XC",90);
+        map.put("CD",400);
+        map.put("CM",900);
+        
+        if(isspecial(s)){
+            return map.get(s);
+        }
+        
+        int ret = 0;
+        int i=0;
+        while(i < s.length()){
+            if(i+1 < s.length() && isspecial(s.substring(i,i+2))){
+                ret += map.get(s.substring(i,i+2));
+                i += 2;
+            } else{
+                ret += map.get(String.valueOf(s.charAt(i)));
+                i ++;
+            }
+        }
+        return ret;        
+    }
+    
+    private boolean isspecial(String s){
+        if(s.equals("IV") || s.equals("IX") || s.equals("XL") ||s.equals("XC")
+          ||s.equals("CD") ||s.equals("CM")){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
