82cc58ec5d0437a607e1eaded4692cb3df4d5027
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2021-09-25 18:45:33 -0700

    Update Solution.java

--- 100. Same Tree/Solution.java
+++ 100. Same Tree/Solution.java
@@ -11,3 +11,19 @@ public class Solution {
         return false;
     }
 }
+
+// v2
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        } 
+        
+        if(p == null && q!= null || (p!=null && q==null) || (p.val != q.val)){
+            return false;
+        }
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+               
+    }
+}
