f0241d6b8aba5f175edc5549c1308621e870139b
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2021-09-25 21:58:53 -0700

    Update Solution1.java

--- 101. Symmetric Tree/Solution1.java
+++ 101. Symmetric Tree/Solution1.java
@@ -32,3 +32,28 @@ class Solution1 {
     }
     
 }
+
+
+
+// v2
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        
+        return helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode left, TreeNode right){
+        if(left == null || right == null){
+            return left == right;
+        }
+        
+        if(left.val != right.val){
+            return false;
+        }
+        
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+}
