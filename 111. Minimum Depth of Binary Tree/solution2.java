435bc32bf355841a8f569d2232b87954b3cdd66d
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2022-01-22 13:42:59 -0800

    Update solution2.java

--- 111. Minimum Depth of Binary Tree/solution2.java
+++ 111. Minimum Depth of Binary Tree/solution2.java
@@ -23,3 +23,33 @@ class Solution {
         return Math.min(helper(root.left) + 1, helper(root.right) + 1);
     }
 }
+
+
+// BFS
+// Runtime: 2 ms, faster than 79.68% 
+// Memory Usage: 92.4 MB, less than 36.31%
+
+class Solution {
+    
+    public int minDepth(TreeNode root) {
+        int ret = 0;
+        if(root == null) return ret;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while(true){
+            ret ++;
+            int size = queue.size();
+            for(int i = 0; i < size; i ++){
+                TreeNode temp = queue.poll();
+                if(temp.left == null && temp.right == null){
+                    return ret;
+                }
+                if(temp.left != null) queue.add(temp.left);
+                if(temp.right != null) queue.add(temp.right);
+            }
+        }
+        
+    }
+    
+}
