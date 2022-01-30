f11b6f217dcfde9099da988296b46194c7c61980
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2022-01-29 21:56:44 -0800

    Update Solution1.java

--- 101. Symmetric Tree/Solution1.java
+++ 101. Symmetric Tree/Solution1.java
@@ -1,4 +1,4 @@
-class Solution1 {   
+  
     // use recursion
 
     // T1 != null && T2!= null
@@ -9,28 +9,20 @@ class Solution1 {
     //Runtime: 0 ms, faster than 100.00%
     //Memory Usage: 39.3 MB, less than 5.08%
 
-    public boolean isSymmetric(TreeNode root) { 
-        
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        
-        return helper(left,right);
+ class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root.left, root.right);
     }
     
     private boolean helper(TreeNode left, TreeNode right){
-        if(left != null && right != null){
-            if(left.val == right.val){
-                return helper(left.left, right.right) && helper(left.right, right.left);} 
-            }else{
-                return false;
-        } else if(left == null && right == null){
+        if(left == null && right == null){
             return true;
-        } else {
+        } else if(left == null || right == null || left.val != right.val){
             return false;
+        } else {
+            return helper(left.left, right.right) && helper(left.right, right.left);
         }
-        
     }
-    
 }
 
 
