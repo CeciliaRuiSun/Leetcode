9945d9d8dae946f59ba3a81229beef87d0429fff
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2022-04-30 08:43:56 -0700

    Update solution2.java

--- 13. Roman to Integer/solution2.java
+++ 13. Roman to Integer/solution2.java
@@ -45,3 +45,32 @@ class Solution {
         }
     }
 }
+
+
+// 
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        int ret = 0;
+        int i = 0;
+        while(i < s.length() - 1){
+            if(map.get(s.charAt(i)) >= map.get(s.charAt(i + 1))){
+                ret += map.get(s.charAt(i));
+            } else{
+                ret -= map.get(s.charAt(i));
+            }
+            i ++;
+        }
+        
+        ret += map.get(s.charAt(s.length() - 1));
+        return ret;
+    }
+}
