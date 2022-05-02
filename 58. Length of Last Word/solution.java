a3c92e6c257ec2c7ebb04edbe6959e1df7e21cf7
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2022-05-01 20:26:43 -0700

    Update solution.java

--- 58. Length of Last Word/solution.java
+++ 58. Length of Last Word/solution.java
@@ -19,3 +19,25 @@ public class solution {
         return count;
     }
 }
+
+
+//
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int ret = 0;
+        int i = s.length() - 1;
+        
+    
+        while(i >= 0 && s.charAt(i) == ' '){
+            i --;
+        }
+            
+        while(i >= 0 && s.charAt(i) != ' '){
+            ret ++;
+            i --;
+        }
+        
+        
+        return ret;
+    }
+}
