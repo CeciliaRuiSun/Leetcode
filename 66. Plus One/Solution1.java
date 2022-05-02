364e0e1fb137f52756f69f97922bb659c27631a9
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2022-05-01 20:51:33 -0700

    Update Solution1.java

--- 66. Plus One/Solution1.java
+++ 66. Plus One/Solution1.java
@@ -1,3 +1,33 @@
+class Solution {
+    public static int[] plusOne(int[] digits) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int carry = 1;
+        for(int i = digits.length - 1; i >= 0; i --){
+            int cur = digits[i];
+            if(cur + carry > 9){
+                arr.add(0, (cur + carry)%10);
+                carry = 1;
+            } else{          
+                arr.add(0, cur + carry);
+                carry = 0;
+            }
+        }
+        
+        if(carry == 1){
+            arr.add(0,1);
+        }
+        
+        int[] ret = new int[arr.size()];
+        for(int i = 0; i < arr.size(); i++){
+            ret[i] = arr.get(i);
+        }
+        
+        return ret;
+    }
+}
+
+
+//
 class Solution1 {
     //use for loop
     //Runtime: 0 ms, faster than 100.00%
